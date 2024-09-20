63e8d6e7006af5684ecb8090ceb0b498e432667b
Claudio López <claudiolopez@MacBook-Pro-de-Claudio.local>
2024-09-20 13:09:33 -0600

    Agregar equipos en main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
+#include "equipo.h"
+
 int main()
 {
+    // Crear equipos
+    Equipo equipo1("Tigres", 5, 1, 15);
+    equipo1.agregar_jugador(Jugador("Carlos", 1.85));
+    equipo1.agregar_jugador(Jugador("Luis", 1.90));
+    equipo1.agregar_jugador(Jugador("Mario", 1.78));
+
+    Equipo equipo2("Águilas", 4, 2, 12);
+    equipo2.agregar_jugador(Jugador("Pedro", 1.88));
+    equipo2.agregar_jugador(Jugador("Juan", 1.75));
 
     return 0;
 }
