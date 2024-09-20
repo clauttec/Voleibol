428124cc4577e41605bd3d81bb15ffc73abd275c
Claudio López <claudiolopez@MacBook-Pro-de-Claudio.local>
2024-09-20 12:50:53 -0600

    Agregar funciones equipo

--- equipo.cpp
+++ equipo.cpp
@@ -7,3 +7,18 @@ void Jugador::imprimir() const
 {
     cout << nombre << ": " << altura << "m" << endl;
 }
+
+// Implementación de la clase Equipo
+Equipo::Equipo(string nombre, int victorias, int derrotas, int puntos)
+    : nombre(nombre), victorias(victorias), derrotas(derrotas), puntos(puntos) {}
+
+void Equipo::agregar_jugador(Jugador jugador)
+{
+    jugadores.push_back(jugador);
+}
+
+void Equipo::imprimir() const
+{
+    cout << nombre << ": " << victorias << " victorias, "
+         << derrotas << " derrotas, " << puntos << " puntos" << endl;
+}
