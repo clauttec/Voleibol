c0fc77fff6e4f87978aed879bf9313c48714b710
Claudio López <claudiolopez@MacBook-Pro-de-Claudio.local>
2024-09-20 12:16:53 -0600

    Agregar clase equipo

--- equipo.h
+++ equipo.h
@@ -16,4 +16,24 @@ public:
     void imprimir() const;
 };
 
+class Equipo
+{
+public:
+    string nombre;
+    int victorias;
+    int derrotas;
+    int puntos;
+    vector<Jugador> jugadores;
+
+    Equipo(string nombre, int victorias, int derrotas, int puntos);
+    void agregar_jugador(Jugador jugador);
+    void imprimir() const;
+};
+
+// Declaración de Quick Sort para ordenar equipos por victorias
+vector<Equipo> quick_sort_equipos(vector<Equipo> equipos);
+
+// Declaración de Merge Sort para ordenar jugadores por altura
+vector<Jugador> merge_sort_jugadores(vector<Jugador> jugadores);
+
 #endif // EQUIPO_H
