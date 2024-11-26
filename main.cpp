e3405fae3377ac0c6fab7566405c7d1af802e75a
Claudio López <claudiolopez@MacBook-Pro-de-Claudio.local>
2024-11-26 05:07:40 -0600

    Actualizar main

--- main.cpp
+++ main.cpp
@@ -13,54 +13,6 @@ int main()
     Equipo equipoVacio;
 
     menu_principal(equipos, opcion, nombre_equipo, nombre_jugador, nombre_archivo, altura, victorias, derrotas, puntos, equipoVacio);
-    /*
-    // Crear un equipo inicial vacio
-    Equipo equipo;
-
-    equipo.cargar_archivo("equipo.txt", equipo);
-
-    // Imprimir el equipo y sus jugadores
-    equipo.imprimir();
-
-    // Eliminar un jugador
-    equipo.jugadores.eliminar_jugador("Pedro");
-
-    // Imprimir el equipo después de eliminar a Pedro
-    cout << "\nDespués de eliminar a Pedro:\n";
-    equipo.imprimir();
-
-    // Prueba de ordenamiento Merge Sort para jugadores
-    vector<Jugador> jugadores = equipo.jugadores.a_vector();
-    jugadores = merge_sort_jugadores(jugadores);
-
-    cout << "\nJugadores ordenados por altura:\n";
-    for (const Jugador &jugador : jugadores)
-    {
-        jugador.imprimir();
-    }
-
-    // Agregar nuevo jugador al equipo
-    equipo.agregar_jugador("Jesus", 1.92);
-
-    // Imprimir equipo despues de agregar a Jesus
-    cout << "\nDespues de agregar a Jesus:\n";
-    equipo.imprimir();
-
-    // Guardar los datos actualizados en el archivo
-    equipo.escribir_en_archivo("equipo_actualizado.txt");
-    */
-
-    /*
-    // Prueba de Quick Sort para equipos
-    vector<Equipo> equipos = {equipo, Equipo("Leones", 8, 3, 28), Equipo("Águilas", 12, 1, 36)};
-    equipos = quick_sort_equipos(equipos);
-
-    cout << "\nEquipos ordenados por victorias:\n";
-    for (const Equipo &eq : equipos)
-    {
-        eq.imprimir();
-    }
-    */
 
     return 0;
 }
